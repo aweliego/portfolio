3933a74fb9097cb00d93385104cb19f2af789af1
aweliego <amelie.gorry.am@gmail.com>
2021-07-20 14:40:15 +0200

    Sticky navbar

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Sticky navbar on scroll
+
+const nav = document.getElementById('navbar');
+
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 50) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+}
+
 // Flip card on click by toggling class
 
 const cards = document.querySelectorAll('.flip-card-inner');
