3a797391616b49a691033f22d978d348befe19bf
aweliego <amelie.gorry.am@gmail.com>
2021-11-28 11:44:13 +0100

    Move hard coded projects list to separate object

---
+++ projects.js
@@ -0,0 +1,79 @@
+export { projects };
+
+const projects = [
+  {
+    title: 'survey-form',
+    imgSrc: 'pics/projects/survey-form.png',
+    description: 'One of the web design projects of freeCodeCamp',
+    tags: '#html-forms #parallax-scrolling-effect #media-queries',
+    link: 'https://aweliego.github.io/survey-form',
+  },
+  {
+    title: 'Product landing page',
+    imgSrc: 'pics/projects/product-landing-page.png',
+    description: 'One of the web design projects of freeCodeCamp',
+    tags: '#flexbox #media-queries',
+    link: 'https://aweliego.github.io/product-landing-page',
+  },
+  {
+    title: 'Technical Documentation page',
+    imgSrc: 'pics/projects/technical-doc.png',
+    description: 'One of the web design projects of freeCodeCamp',
+    tags: '#css-grids #media-queries',
+    link: 'https://aweliego.github.io/technical-documentation-page',
+  },
+  {
+    title: 'Bootstrap site',
+    imgSrc: 'pics/projects/bootstrap.png',
+    description: 'A simple website to explore Bootstrap features',
+    tags: '#bootstrap4<',
+    link: 'https://amsterdam-off-the-beaten-track.netlify.app/',
+  },
+  {
+    title: 'Balloon popping game',
+    imgSrc: 'pics/projects/balloon-game.png',
+    description:
+      "The project I built during a coding introduction workshop (so not great on mobile I'm afraid!). It was fun to figure this one out though, so it gets a spot for now!",
+    tags: '',
+    link: 'https://balloon-popping-game.netlify.app/',
+  },
+  {
+    title: 'Company landing page',
+    imgSrc: 'pics/projects/company-landing-page.png',
+    description: 'One of my first attempts at making a responsive website',
+    tags: '#flexbox #media-queries',
+    link: 'https://eventful-landing-page.netlify.app/',
+  },
+  {
+    title: 'Javascript game',
+    imgSrc: 'pics/projects/chore-door.png',
+    description:
+      'A Codecademy project which made me explore the power of Javascript events!',
+    tags: '#javascript-game #dom-events',
+    link: 'https://chore-door-js.netlify.app/',
+  },
+  {
+    title: 'Hoverboard',
+    imgSrc: '/pics/projects/hoverboard.png',
+    description:
+      'A fun mini "drawing" app from <a href="https://github.com/bradtraversy">@bradtraversy</a>, to which I added a color scheme picker and some responsiveness',
+    tags: '',
+    link: 'https://hoverboard-color-scheme.netlify.app/',
+  },
+  {
+    title: 'Notes app',
+    imgSrc: '/pics/projects/notes-app.png',
+    description:
+      'Another project from <a href="https://github.com/bradtraversy">@bradtraversy</a> which I gave some extra features to, including a dark theme!',
+    tags: '',
+    link: 'https://notes-app-makeover.netlify.app/',
+  },
+  {
+    title: 'Javascript programs',
+    imgSrc: '/pics/projects/js-projects.png',
+    description:
+      'I\'ve also completed many other JS "open requirements" projects, whose code is available on my GitHub!',
+    tags: '',
+    link: 'https://github.com/aweliego?tab=repositories&q=&type=&language=javascript',
+  },
+];
