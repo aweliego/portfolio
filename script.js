83c8b60e777943b7fb77ffa47e3b239044cd39e4
aweliego <amelie.gorry.am@gmail.com>
2021-11-28 11:45:12 +0100

    Import projects from object and create flipcards dynamically + some refactoring

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Sticky navbar on scroll
+import { projects } from './projects.js';
 
 const nav = document.getElementById('navbar');
+const innerFlipCards = document.querySelectorAll('.flip-card-inner');
 
 window.addEventListener('scroll', fixNav);
+createProjectCard(projects);
 
+// Sticky navbar on scroll
 function fixNav() {
   if (window.scrollY > nav.offsetHeight + 50) {
     nav.classList.add('active');
@@ -12,11 +15,40 @@ function fixNav() {
   }
 }
 
+// Import projects dynamically and create flipcards
+function createProjectCard(projects) {
+  projects.forEach((project) => {
+    const { title, imgSrc, description, tags, link } = project;
+    const flipCardContainer = document.querySelector('.flip-card-container');
+    const flipCard = document.createElement('div');
+    flipCard.classList.add('flip-card', 'animated-bg');
+    flipCard.innerHTML = `<div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="${imgSrc}"
+                alt="Preview project"
+                class="project-img"
+              />
+            </div>
+            <div class="flip-card-back">
+              <h3>${title}</h3>
+              <p>${description}</p>
+              <p>${tags}</p>
+              <a
+                href="${link}"
+                target="_blank"
+                class="btn"
+                >Check it out
+              </a>
+            </div>
+          </div>`;
+
+    flipCardContainer.append(flipCard);
+  });
+}
+
 // Flip card on click by toggling class
-
-const cards = document.querySelectorAll('.flip-card-inner');
-
-cards.forEach(function (card) {
+innerFlipCards.forEach((card) => {
   card.addEventListener('click', (event) => {
     const card = event.currentTarget;
     card.classList.toggle('is-flipped');
