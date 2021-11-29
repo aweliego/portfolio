d4bf022db4b2f143bd8b96b93464c1bdc4a13ed7
aweliego <amelie.gorry.am@gmail.com>
2021-11-29 22:16:09 +0100

    Add loading animation of project cards and fix flip effects on click/mobile

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import { projects } from './projects.js';
 
 const nav = document.getElementById('navbar');
-const innerFlipCards = document.querySelectorAll('.flip-card-inner');
 
 window.addEventListener('scroll', fixNav);
 createProjectCard(projects);
@@ -19,38 +18,47 @@ function fixNav() {
 function createProjectCard(projects) {
   projects.forEach((project) => {
     const { title, imgSrc, description, tags, link } = project;
+
+    // Create flip-cards
     const flipCardContainer = document.querySelector('.flip-card-container');
     const flipCard = document.createElement('div');
     flipCard.classList.add('flip-card', 'animated-bg');
-    flipCard.innerHTML = `<div class="flip-card-inner">
-            <div class="flip-card-front">
-              <img
-                src="${imgSrc}"
-                alt="Preview project"
-                class="project-img"
-              />
-            </div>
-            <div class="flip-card-back">
-              <h3>${title}</h3>
-              <p>${description}</p>
-              <p>${tags}</p>
-              <a
-                href="${link}"
-                target="_blank"
-                class="btn"
-                >Check it out
-              </a>
-            </div>
-          </div>`;
-
     flipCardContainer.append(flipCard);
+
+    // Create innerFlipCards
+    const innerFlipCard = document.createElement('div');
+    innerFlipCard.classList.add('flip-card-inner');
+
+    // Add event-listener
+    innerFlipCard.addEventListener('click', (event) => {
+      const card = event.currentTarget;
+      card.classList.toggle('is-flipped');
+    });
+
+    // Show cards content after 1.5s
+    setTimeout(() => {
+      innerFlipCard.innerHTML = `<div class="flip-card-front">
+        <img
+          src="${imgSrc}"
+          alt="Preview project"
+          class="project-img"
+        />
+      </div>
+      <div class="flip-card-back">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>${tags}</p>
+        <a
+          href="${link}"
+          target="_blank"
+          class="btn"
+          >Check it out
+        </a>
+      </div>`;
+
+      flipCard.classList.remove('animated-bg');
+    }, 1500);
+
+    flipCard.append(innerFlipCard);
   });
 }
-
-// Flip card on click by toggling class
-innerFlipCards.forEach((card) => {
-  card.addEventListener('click', (event) => {
-    const card = event.currentTarget;
-    card.classList.toggle('is-flipped');
-  });
-});
