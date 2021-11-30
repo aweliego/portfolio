285db56ce12e0c83712c1993c0916415e8dd1ba5
aweliego <amelie.gorry.am@gmail.com>
2021-11-30 13:08:23 +0100

    Create project cards on scroll + refactor fixNav()

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 import { projects } from './projects.js';
 
 const nav = document.getElementById('navbar');
+const scrollElement = document.getElementById('projects');
+const scrollOffset = 100;
 
 window.addEventListener('scroll', fixNav);
-createProjectCard(projects);
+window.addEventListener('scroll', handleScrollAnimation);
 
 // Sticky navbar on scroll
 function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 50) {
-    nav.classList.add('active');
-  } else {
-    nav.classList.remove('active');
+  window.scrollY > nav.offsetHeight + 50
+    ? nav.classList.add('active')
+    : nav.classList.remove('active');
+}
+
+// Create projects cards on scroll
+function elementInView(el, offset = 0) {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) - offset
+  );
+}
+
+function handleScrollAnimation() {
+  if (elementInView(scrollElement, scrollOffset)) {
+    createProjectCard(projects);
   }
 }
 
@@ -38,23 +54,23 @@ function createProjectCard(projects) {
     // Show cards content after 1.5s
     setTimeout(() => {
       innerFlipCard.innerHTML = `<div class="flip-card-front">
-        <img
-          src="${imgSrc}"
-          alt="Preview project"
-          class="project-img"
-        />
-      </div>
-      <div class="flip-card-back">
-        <h3>${title}</h3>
-        <p>${description}</p>
-        <p>${tags}</p>
-        <a
-          href="${link}"
-          target="_blank"
-          class="btn"
-          >Check it out
-        </a>
-      </div>`;
+          <img
+            src="${imgSrc}"
+            alt="Preview project"
+            class="project-img"
+          />
+        </div>
+        <div class="flip-card-back">
+          <h3>${title}</h3>
+          <p>${description}</p>
+          <p>${tags}</p>
+          <a
+            href="${link}"
+            target="_blank"
+            class="btn"
+            >Check it out
+          </a>
+        </div>`;
 
       flipCard.classList.remove('animated-bg');
     }, 1500);
