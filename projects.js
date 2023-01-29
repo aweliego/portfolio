af2eeb7751afbb8415c4aa102855727f447f6eb3
aweliego <amelie.gorry.am@gmail.com>
2023-01-29 10:53:00 +0100

    Update project list and add React icon

--- projects.js
+++ projects.js
@@ -1,6 +1,70 @@
-export { projects };
+export { projects }
 
 const projects = [
+  {
+    title: 'Intro section with dropdown navigation',
+    imgSrc: '/pics/projects/intro-section.png',
+    description:
+      '<a href="https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5">Frontend Mentor challenge #3</a>: I used this challenge to explore the Material UI library and strengthen my React and TypeScript skills.',
+    tags: '#react #typescript #material-ui',
+    link: 'https://intro-section-with-drop-down-nav.netlify.app/',
+  },
+  {
+    title: 'Time tracking dashboard',
+    imgSrc: '/pics/projects/dashboard.png',
+    description:
+      '<a href="https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw">Frontend Mentor challenge #2</a>: the challenge here lied in the responsive layout and rendering the data dynamically',
+    tags: '#react #css-grid #semantic-markup',
+    link: 'https://time-tracking-dashboard-challenge.netlify.app/',
+  },
+  {
+    title: 'Interactive rating tool',
+    imgSrc: '/pics/projects/rating-tool.png',
+    description:
+      '<a href="https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI">Frontend Mentor challenge #1</a>: recreating this survey tool according to the provided design. Practicing for the first time with TypeScript and Sass!',
+    tags: '#typescript #sass #dom-manipulation #semantic-markup',
+    link: 'https://interactive-rating-tool.netlify.app/',
+  },
+  {
+    title: 'Film finder',
+    imgSrc: '/pics/projects/film-finder.png',
+    description:
+      'A Codecademy project to which I added a bunch of features and improved the styles - mostly playing with the Movie Database API and local storage',
+    tags: '#javascript #api #responsiveness #localstorage',
+    link: 'https://film-finder-app.netlify.app/',
+  },
+  {
+    title: 'Secret Santa',
+    imgSrc: '/pics/projects/secret-santa.png',
+    description:
+      'The main objective was to write the algorithm, the UI was for the fun! (Pch, the typing out effect you see is from <a href="https://github.com/bradtraversy">@bradtraversy</a>)',
+    tags: '#javascript #dom-events',
+    link: 'https://secret-santa-app.netlify.app/',
+  },
+  {
+    title: 'Javascript game',
+    imgSrc: 'pics/projects/chore-door.png',
+    description:
+      'A Codecademy project which made me explore the power of Javascript events!',
+    tags: '#javascript-game #dom-events',
+    link: 'https://chore-door-js.netlify.app/',
+  },
+  {
+    title: 'Recipe generator',
+    imgSrc: '/pics/projects/recipe-app.png',
+    description:
+      'A dynamic recipe app that uses the <a href="https://spoonacular.com/food-api">spoonacular food API</a> - a work still in progress to add the possibility to save a recipe!',
+    tags: '#javascript #api #dom-manipulation',
+    link: 'https://recipe-loader.netlify.app/',
+  },
+  {
+    title: 'Notes app',
+    imgSrc: '/pics/projects/notes-app.png',
+    description:
+      'Another project from <a href="https://github.com/bradtraversy">@bradtraversy</a> which I gave some extra features to, including a dark theme!',
+    tags: '',
+    link: 'https://notes-app-makeover.netlify.app/',
+  },
   {
     title: 'Survey form',
     imgSrc: 'pics/projects/survey-form.png',
@@ -23,65 +87,10 @@ const projects = [
     link: 'https://aweliego.github.io/technical-documentation-page',
   },
   {
-    title: 'Bootstrap site',
-    imgSrc: 'pics/projects/bootstrap.png',
-    description: 'A simple website to explore Bootstrap features',
-    tags: '#bootstrap4',
-    link: 'https://amsterdam-off-the-beaten-track.netlify.app/',
-  },
-  {
     title: 'Company landing page',
     imgSrc: 'pics/projects/company-landing-page.png',
     description: 'One of my first attempts at making a responsive website',
     tags: '#flexbox #media-queries',
     link: 'https://eventful-landing-page.netlify.app/',
   },
-  {
-    title: 'Javascript game',
-    imgSrc: 'pics/projects/chore-door.png',
-    description:
-      'A Codecademy project which made me explore the power of Javascript events!',
-    tags: '#javascript-game #dom-events',
-    link: 'https://chore-door-js.netlify.app/',
-  },
-  {
-    title: 'Hoverboard',
-    imgSrc: '/pics/projects/hoverboard.png',
-    description:
-      'A fun mini "drawing" app from <a href="https://github.com/bradtraversy">@bradtraversy</a>, to which I added a color scheme picker and some responsiveness',
-    tags: '',
-    link: 'https://hoverboard-color-scheme.netlify.app/',
-  },
-  {
-    title: 'Notes app',
-    imgSrc: '/pics/projects/notes-app.png',
-    description:
-      'Another project from <a href="https://github.com/bradtraversy">@bradtraversy</a> which I gave some extra features to, including a dark theme!',
-    tags: '',
-    link: 'https://notes-app-makeover.netlify.app/',
-  },
-  {
-    title: 'Recipe generator',
-    imgSrc: '/pics/projects/recipe-app.png',
-    description:
-      'A dynamic recipe app that uses the <a href="https://spoonacular.com/food-api">spoonacular food API</a> - a work still in progress to add the possibility to save a recipe!',
-    tags: '#javascript #api #dom-manipulation',
-    link: 'https://recipe-loader.netlify.app/',
-  },
-  {
-    title: 'Secret Santa',
-    imgSrc: '/pics/projects/secret-santa.png',
-    description:
-      'The main objective was to write the algorithm, the UI was for the fun! (Pch, the typing out effect you see is from <a href="https://github.com/bradtraversy">@bradtraversy</a>)',
-    tags: '#javascript #dom-events',
-    link: 'https://secret-santa-app.netlify.app/',
-  },
-  {
-    title: 'Film finder',
-    imgSrc: '/pics/projects/film-finder.png',
-    description:
-      'A Codecademy project to which I added a bunch of features and improved the styles - mostly playing with the Movie Database API and local storage',
-    tags: '#javascript #api #responsiveness #localstorage',
-    link: 'https://film-finder-app.netlify.app/',
-  },
-];
+]
