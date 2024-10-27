1dab3c600fe96326331f961623fdd1624941c88b
“Amélie <amelie.gorry.am@gmail.com>
2024-10-27 11:10:47 +0100

    Update projects

--- projects.js
+++ projects.js
@@ -2,12 +2,28 @@ export { projects }
 
 const projects = [
   {
+    title: 'Interactive comment section',
+    imgSrc: '/pics/projects/comment-section.png',
+    description:
+      '<a href="https://www.frontendmentor.io/solutions/interactive-comment-section-with-react-typescript-and-tailwindcss-grds2rcFdt">Frontend Mentor challenge #5</a>: a typical CRUD app to which I added some user requirements and unit tests!',
+    tags: '#react #typescript #tailwind-css #crud #vitest',
+    link: 'https://interactive-comment-section-fem-challenge.netlify.app/',
+  },
+  {
+    title: 'Job listing filtering page',
+    imgSrc: '/pics/projects/job-listings.png',
+    description:
+      '<a href="https://www.frontendmentor.io/solutions/job-listings-filtering-page-with-react-typescript-and-tailwindcss-mOLRJdOdtu">Frontend Mentor challenge #4</a>: this one was all about filtering items based on categories - playing with Tailwind CSS for the first time!',
+    tags: '#react #typescript #tailwind-css',
+    link: 'https://job-listings-fem-challenge.netlify.app/',
+  },
+  {
     title: 'Intro section with dropdown navigation',
     imgSrc: '/pics/projects/intro-section.png',
     description:
       '<a href="https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5">Frontend Mentor challenge #3</a>: I used this challenge to explore the Material UI library and strengthen my React and TypeScript skills.',
     tags: '#react #typescript #material-ui',
-    link: 'https://intro-section-with-drop-down-nav.netlify.app/',
+    link: 'https://interactive-comment-section-fem-challenge.netlify.app/',
   },
   {
     title: 'Time tracking dashboard',
@@ -41,56 +57,56 @@ const projects = [
     tags: '#javascript #dom-events',
     link: 'https://secret-santa-app.netlify.app/',
   },
-  {
-    title: 'Javascript game',
-    imgSrc: 'pics/projects/chore-door.png',
-    description:
-      'A Codecademy project which made me explore the power of Javascript events!',
-    tags: '#javascript-game #dom-events',
-    link: 'https://chore-door-js.netlify.app/',
-  },
-  {
-    title: 'Recipe generator',
-    imgSrc: '/pics/projects/recipe-app.png',
-    description:
-      'A dynamic recipe app that uses the <a href="https://spoonacular.com/food-api">spoonacular food API</a> - a work still in progress to add the possibility to save a recipe!',
-    tags: '#javascript #api #dom-manipulation',
-    link: 'https://recipe-loader.netlify.app/',
-  },
-  {
-    title: 'Notes app',
-    imgSrc: '/pics/projects/notes-app.png',
-    description:
-      'Another project from <a href="https://github.com/bradtraversy">@bradtraversy</a> which I gave some extra features to, including a dark theme!',
-    tags: '',
-    link: 'https://notes-app-makeover.netlify.app/',
-  },
-  {
-    title: 'Survey form',
-    imgSrc: 'pics/projects/survey-form.png',
-    description: 'One of the web design projects of freeCodeCamp',
-    tags: '#html-forms #parallax-scrolling-effect #media-queries',
-    link: 'https://aweliego.github.io/survey-form',
-  },
-  {
-    title: 'Product landing page',
-    imgSrc: 'pics/projects/product-landing-page.png',
-    description: 'One of the web design projects of freeCodeCamp',
-    tags: '#flexbox #media-queries',
-    link: 'https://aweliego.github.io/product-landing-page',
-  },
-  {
-    title: 'Technical documentation page',
-    imgSrc: 'pics/projects/technical-doc.png',
-    description: 'One of the web design projects of freeCodeCamp',
-    tags: '#css-grids #media-queries',
-    link: 'https://aweliego.github.io/technical-documentation-page',
-  },
-  {
-    title: 'Company landing page',
-    imgSrc: 'pics/projects/company-landing-page.png',
-    description: 'One of my first attempts at making a responsive website',
-    tags: '#flexbox #media-queries',
-    link: 'https://eventful-landing-page.netlify.app/',
-  },
+  //   {
+  //     title: 'Javascript game',
+  //     imgSrc: 'pics/projects/chore-door.png',
+  //     description:
+  //       'A Codecademy project which made me explore the power of Javascript events!',
+  //     tags: '#javascript-game #dom-events',
+  //     link: 'https://chore-door-js.netlify.app/',
+  //   },
+  //   {
+  //     title: 'Recipe generator',
+  //     imgSrc: '/pics/projects/recipe-app.png',
+  //     description:
+  //       'A dynamic recipe app that uses the <a href="https://spoonacular.com/food-api">spoonacular food API</a> - a work still in progress to add the possibility to save a recipe!',
+  //     tags: '#javascript #api #dom-manipulation',
+  //     link: 'https://recipe-loader.netlify.app/',
+  //   },
+  //   {
+  //     title: 'Notes app',
+  //     imgSrc: '/pics/projects/notes-app.png',
+  //     description:
+  //       'Another project from <a href="https://github.com/bradtraversy">@bradtraversy</a> which I gave some extra features to, including a dark theme!',
+  //     tags: '',
+  //     link: 'https://notes-app-makeover.netlify.app/',
+  //   },
+  //   {
+  //     title: 'Survey form',
+  //     imgSrc: 'pics/projects/survey-form.png',
+  //     description: 'One of the web design projects of freeCodeCamp',
+  //     tags: '#html-forms #parallax-scrolling-effect #media-queries',
+  //     link: 'https://aweliego.github.io/survey-form',
+  //   },
+  //   {
+  //     title: 'Product landing page',
+  //     imgSrc: 'pics/projects/product-landing-page.png',
+  //     description: 'One of the web design projects of freeCodeCamp',
+  //     tags: '#flexbox #media-queries',
+  //     link: 'https://aweliego.github.io/product-landing-page',
+  //   },
+  //   {
+  //     title: 'Technical documentation page',
+  //     imgSrc: 'pics/projects/technical-doc.png',
+  //     description: 'One of the web design projects of freeCodeCamp',
+  //     tags: '#css-grids #media-queries',
+  //     link: 'https://aweliego.github.io/technical-documentation-page',
+  //   },
+  //   {
+  //     title: 'Company landing page',
+  //     imgSrc: 'pics/projects/company-landing-page.png',
+  //     description: 'One of my first attempts at making a responsive website',
+  //     tags: '#flexbox #media-queries',
+  //     link: 'https://eventful-landing-page.netlify.app/',
+  //   },
 ]
